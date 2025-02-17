8b9216f3bf7480b46b42238c8ef9ce123ccc4652
EllisOsen <ellisfinnosen@gmail.com>
2025-02-17 16:12:48 -0600

    update Google Tag Manager ID in bakery.js

--- bakery/bakery.js
+++ bakery/bakery.js
@@ -16,7 +16,7 @@
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KLKWF4SJ');
+            })(window,document,'script','dataLayer','GTM-N98BRFV7');
           `;
     document.head.appendChild(gtmScript);
     console.log("GTM SCript added");
@@ -24,7 +24,7 @@
     // 3. Add Google Tag Manager <noscript> in the <body>
     const gtmNoscript = document.createElement("noscript");
     gtmNoscript.innerHTML = `
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KLKWF4SJ"
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N98BRFV7"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>
           `;
     document.body.insertAdjacentElement("afterbegin", gtmNoscript);
